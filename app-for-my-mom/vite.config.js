8a7986b22a9495337bb4f315b2478c7da9b22aaf
vferat <victor.ferat@live.fr>
2024-09-27 21:28:23 +0200

    add PWA

--- app-for-my-mom/vite.config.js
+++ app-for-my-mom/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/app-for-my-mom/'
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }})
+  ],
 })
